9e3f191adf61c3cc0b80cdd6fda3cde31fc27931
獨孤飛 <dugufei@bjtu.edu.cn>
2021-05-19 23:56:58 +0800

    topk

--- others/cpp/topk.cpp
+++ others/cpp/topk.cpp
@@ -1,28 +1,68 @@
+/*
+ * @Author: dugufei (dugufei@bjtu.edu.cn)
+ * @Date:   2021-05-19 22:51:50
+ * @Last Modified by:   dugufei (dugufei@bjtu.edu.cn)
+ * @Last Modified time: 2021-05-19 23:56:19
+ */
+
 #include <iostream>
 #include <queue>
 
-int main() {
-    int k = 0;
-    std::cin >> k;
-    int tmp = 0;
-    int i = 0;
-    std::priority_queue<int, std::vector<int>, std::greater<int>> min_heap;
-    while (std::cin >> tmp) {
+template<typename RandomIterator, typename Compare = std::greater<typename RandomIterator::value_type>>
+inline std::vector<typename RandomIterator::value_type>
+topk(RandomIterator first, RandomIterator last, size_t k, Compare compare = Compare()) {
+    std::priority_queue<typename RandomIterator::value_type, std::vector<typename RandomIterator::value_type>, Compare> heap;
+    std::vector<typename RandomIterator::value_type> res;
+    size_t i = 0;
+    while (first != last) {
         if (i++ < k) {
-            min_heap.push(tmp);
+            heap.push(*first);
+            first++;
             continue;
         }
-        if (tmp <= min_heap.top()) {
+        if (!(compare(*first, heap.top()))) {
+            first++;
             continue;
         }
-        min_heap.pop();
-        min_heap.push(tmp);
+        heap.pop();
+        heap.push(*first);
+        first++;
     }
-    while (!min_heap.empty()) {
-        std::cout << min_heap.top() << std::endl;
-        min_heap.pop();
+    while (!heap.empty()) {
+        res.push_back(heap.top());
+        heap.pop();
     }
-    std::cout << "Hello world" << std::endl;
+    return res;
+}
+
+int main() {
+    std::vector<int32_t> in = {4, 2, 5, 3, 5, 1, 4, 6, 7, 8, 9};
+    auto out = topk(in.begin(), in.end(), 4);
+    for (auto i : out) {
+        std::cout << i << std::endl;
+    }
+
+    std::cout << "-------------" << std::endl;
+
+    out = topk(in.begin(), in.end(), 4, std::less<int32_t>());
+    for (auto i : out) {
+        std::cout << i << std::endl;
+    }
+    
+    std::cout << "-------------" << std::endl;
+
+    std::string str = "adsffagdfa";
+    auto str_out = topk(str.begin(), str.end(), 4);
+    for (auto i : str_out) {
+        std::cout << i << std::endl;
+    }
+    
+    std::cout << "-------------" << std::endl;
+
+    str_out = topk(str.begin(), str.end(), 4, std::less<char>());
+    for (auto i : str_out) {
+        std::cout << i << std::endl;
+    }
     return 0;
 }
 
