4e22cbad5c6ef516d394bd429eba5b6c09f2aea7
liutengfei04 <liutengfei04@baidu.com>
2024-03-10 15:48:34 +0800

    a

---
+++ leetcode/00242_valid_anagram/valid_anagram.cpp
@@ -0,0 +1,42 @@
+/*
+ * @Author:             dugufei (dugufei@bjtu.edu.cn)
+ * @Date:               2021-05-18 20:33:02
+ * @Last Modified by:   dugufei (dugufei@bjtu.edu.cn)
+ * @Last Modified time: 2022-03-16 00:02:17
+ */
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) {
+            return false;
+        }
+        vector<int> char_set(26, 0);
+        for (int i = 0; i < s.size(); ++i) {
+            char_set[s[i] - 'a']++;
+            char_set[t[i] - 'a']--;
+        }
+        for (auto i : char_set) {
+            if (i != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+int main() {
+    Solution solution;
+    std::cout << solution.isAnagram("", "") << std::endl;
+    std::cout << solution.isAnagram("anagram", "nagaram") << std::endl;
+    std::cout << solution.isAnagram("rat", "car") << std::endl;
+    return 0;
+}
+
+// clang++ -std=c++2a valid_anagram.cpp
